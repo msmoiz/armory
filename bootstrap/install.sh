11950851449ff2897bba461202cb35ab4cf64d53
msmoiz <mustafa.moiz125@gmail.com>
2024-09-23 10:13:06 -0700

    feat: update install script to be cross-platform

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -2,32 +2,50 @@
 
 set -e
 
+main() {
+    arch=$(architecture)
+    os=$(operating_system)
+
+    info "detected platform | arch: ${arch} | os: ${os}"
+
+    # Download the binary
+    armory_bin="armory_bootstrap"
+
+    info "downloading armory"
+    curl "https://armory.msmoiz.com/download/armory-${arch}-${os}" \
+        --output "${armory_bin}" \
+        --fail \
+        --silent
+    info "downloaded armory"
+
+    # Make it executable
+    chmod +x "${armory_bin}"
+
+    # Create the armory home and binary dirs
+    armory_home="${HOME}/.armory"
+    armory_home_bin="${armory_home}/bin"
+    if [ ! -d "${armory_home_bin}" ]; then
+        mkdir -p "${armory_home_bin}"
+    fi
+
+    # Install the binary to the correct location
+    install_path="${armory_home_bin}/armory"
+    mv "${armory_bin}" "${install_path}"
+
+    info "installed armory to ${install_path}"
+    info "add ${armory_home_bin} to path to complete installation"
+}
+
 info() {
     echo "install: $@"
 }
 
-# Download the binary
-armory_bin="armory_bootstrap"
+architecture() {
+    arch
+}
 
-info "downloading armory"
-curl https://armory.msmoiz.com/download/armory \
-    --output "${armory_bin}" \
-    --silent
-info "downloaded armory"
+operating_system() {
+    uname | tr '[:upper:]' '[:lower:]'
+}
 
-# Make it executable
-chmod +x "${armory_bin}"
-
-# Create the armory home and binary dirs
-armory_home="${HOME}/.armory"
-armory_home_bin="${armory_home}/bin"
-if [ ! -d "${armory_home_bin}" ]; then
-    mkdir -p "${armory_home_bin}"
-fi
-
-# Install the binary to the correct location
-install_path="${armory_home_bin}/armory"
-mv "${armory_bin}" "${install_path}"
-
-info "installed armory to ${install_path}"
-info "add ${armory_home_bin} to path to complete installation"
+main
