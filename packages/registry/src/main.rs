5ae3632d727250bd2e63586f7360b07b56f2d275
msmoiz <mustafa.moiz125@gmail.com>
2026-07-30 21:26:55 -0700

    extract version sort to utils package

--- packages/registry/src/main.rs
+++ packages/registry/src/main.rs
@@ -1,5 +1,4 @@
 use std::{
-    cmp::Ordering,
     env::VarError,
     fs,
     path::{Path, PathBuf},
@@ -24,6 +23,7 @@ use serde::Serialize;
 use sha2::{Digest, Sha256};
 use tower_http::trace::TraceLayer;
 use tracing::{error, info, warn};
+use utils::sort_versions;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -300,62 +300,6 @@ async fn get_info(
         name: input.name,
         versions,
     }))
-}
-
-/// Sorts version strings in ascending order.
-fn sort_versions(a: &String, b: &String) -> Ordering {
-    let mut a = a.split(".");
-    let mut b = b.split(".");
-    loop {
-        match (a.next(), b.next()) {
-            (None, None) => return Ordering::Equal,
-            (None, Some(_b)) => return Ordering::Less,
-            (Some(_a), None) => return Ordering::Greater,
-            (Some(a), Some(b)) => {
-                let a = a.parse::<u8>().expect("version part should be integer");
-                let b = b.parse::<u8>().expect("version part should be integer");
-
-                if a < b {
-                    return Ordering::Less;
-                }
-
-                if a > b {
-                    return Ordering::Greater;
-                }
-            }
-        }
-    }
-}
-
-#[cfg(test)]
-mod get_info_tests {
-    use crate::sort_versions;
-
-    #[test]
-    fn version_sort() {
-        let mut versions = vec![
-            String::from("1.0"),
-            String::from("2.0.1"),
-            String::from("1.10.0"),
-            String::from("1.9.0"),
-            String::from("1.0.0"),
-            String::from("2"),
-        ];
-
-        versions.sort_by(sort_versions);
-
-        assert_eq!(
-            versions,
-            [
-                String::from("1.0"),
-                String::from("1.0.0"),
-                String::from("1.9.0"),
-                String::from("1.10.0"),
-                String::from("2"),
-                String::from("2.0.1"),
-            ]
-        )
-    }
 }
 
 async fn list(
